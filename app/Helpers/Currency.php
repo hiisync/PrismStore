51eb146b89b1043de9ac3c1ad688f72e474808d8
Steven Stevenson <129100929+duckdaw@users.noreply.github.com>
2023-07-08 03:35:09 +0300

    Replacing the exchange rate API with another one.

--- app/Helpers/Currency.php
+++ app/Helpers/Currency.php
@@ -3,14 +3,29 @@
 if (! function_exists('convertCurrency')) {
     function convertCurrency($amount, $from_currency, $to_currency)
     {
-        $url = "https://api.exchangerate-api.com/v4/latest/$from_currency";
+        $url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
         $exchange_rates = file_get_contents($url);
         $exchange_rates = json_decode($exchange_rates, true);
 
-        $rate = $exchange_rates['rates'][$to_currency];
+        $rate = findExchangeRate($exchange_rates, $from_currency, $to_currency);
+
+        if ($rate === null) {
+            return null; // Return null if the conversion rate is not found
+        }
 
         $converted_amount = $amount * $rate;
 
         return $converted_amount;
+    }
+
+    function findExchangeRate($exchange_rates, $from_currency, $to_currency)
+    {
+        foreach ($exchange_rates as $rate) {
+            if ($rate['cc'] === $from_currency && $rate['cc'] !== $to_currency) {
+                return $rate['rate'];
+            }
+        }
+
+        return null;
     }
 }
